7150774922e04f6d1c258d296a59b305032515ba
David R <davidrubio97@outlook.com>
2024-02-24 18:36:16 +0100

    update

---
+++ FH1223/ibert_ultrascale_gth_z7_p_ex/ibert_ultrascale_gth_0_ex.runs/impl_1/runme.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+# 
+# Vivado(TM)
+# runme.sh: a Vivado-generated Runs Script for UNIX
+# Copyright 1986-2020 Xilinx, Inc. All Rights Reserved.
+# 
+
+echo "This script was generated under a different operating system."
+echo "Please update the PATH and LD_LIBRARY_PATH variables below, before executing this script"
+exit
+
+if [ -z "$PATH" ]; then
+  PATH=F:/xilinx_2020.1/Vitis/2020.1/bin;F:/xilinx_2020.1/Vivado/2020.1/ids_lite/ISE/bin/nt64;F:/xilinx_2020.1/Vivado/2020.1/ids_lite/ISE/lib/nt64:F:/xilinx_2020.1/Vivado/2020.1/bin
+else
+  PATH=F:/xilinx_2020.1/Vitis/2020.1/bin;F:/xilinx_2020.1/Vivado/2020.1/ids_lite/ISE/bin/nt64;F:/xilinx_2020.1/Vivado/2020.1/ids_lite/ISE/lib/nt64:F:/xilinx_2020.1/Vivado/2020.1/bin:$PATH
+fi
+export PATH
+
+if [ -z "$LD_LIBRARY_PATH" ]; then
+  LD_LIBRARY_PATH=
+else
+  LD_LIBRARY_PATH=:$LD_LIBRARY_PATH
+fi
+export LD_LIBRARY_PATH
+
+HD_PWD='b:/Z7_P/FH1159_Z7_P_demo/vivado/fh1159_z9es1_demo/ibert_ultrascale_gth_0_ex/ibert_ultrascale_gth_0_ex.runs/impl_1'
+cd "$HD_PWD"
+
+HD_LOG=runme.log
+/bin/touch $HD_LOG
+
+ISEStep="./ISEWrap.sh"
+EAStep()
+{
+     $ISEStep $HD_LOG "$@" >> $HD_LOG 2>&1
+     if [ $? -ne 0 ]
+     then
+         exit
+     fi
+}
+
+# pre-commands:
+/bin/touch .init_design.begin.rst
+EAStep vivado -log example_ibert_ultrascale_gth_0.vdi -applog -m64 -product Vivado -messageDb vivado.pb -mode batch -source example_ibert_ultrascale_gth_0.tcl -notrace
+
+
